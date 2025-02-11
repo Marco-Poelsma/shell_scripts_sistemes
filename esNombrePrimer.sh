f6878158422f3a377410f25463a5815e1d50cfcd
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-02-11 11:57:45 +0100

    Hotfix ex. 5

--- esNombrePrimer.sh
+++ esNombrePrimer.sh
@@ -3,6 +3,11 @@ if [ $1 -le 1 ]; then
     exit 1
 fi
 
+if [ $1 -eq 2 ]; then
+    echo "Es primer"
+    exit 0
+fi
+
 for i in $(seq 2 $(($1/2+1))) # 2 a n/2+1. No fa falta comprovar per nombres més grans ja que a partir de n/2+1 els factors que s'obtenen són factors que es poden multiplicar per factors ja comprovats
 do
     if [ $(($1 % $i)) -eq 0 ]; then
